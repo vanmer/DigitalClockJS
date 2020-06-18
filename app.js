c7323049210f338250accf24982c11e79bea74cd
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-18 12:25:57 +0200

    two digits, starting css

--- app.js
+++ app.js
@@ -8,29 +8,34 @@ document.addEventListener('DOMContentLoaded', () => {
     var h = date.getHours();      // 0 - 23
     var m = date.getMinutes();    // 0 - 59
     var s = date.getSeconds();    // 0 - 59
+    var session = "AM";
 
     if (h == 0) {
       h = 12;
     }
     else if (h > 12) {
       h = h - 12;
+      session = "PM";
     }
     // else if (h < 10) {
-    //   h = "0" + h;    // show two digits for hours
+    //   h = "0" + h;
     // }
 
     // if (m < 10) {
-    //   m = "0" + m;    // show two digits for minutes
+    //   m = "0" + m;
     // }
     //
     // if (s < 10) {
-    //   s = "0" + s;    // show two digits for minutes
+    //   s = "0" + s;
     // }
 
-    
+    h = (h < 10) ? "0" + h : h;   // show two digits for hours
+    m = (m < 10) ? "0" + m : m;   // show two digits for minutes
+    s = (s < 10) ? "0" + s : s;   // show two digits for minutes
 
 
-    var time = h + ":" + m + ":" + s;
+    // var time = h + ":" + m + ":" + s + " " + session;
+    var time = `${h}:${m}:${s} ${session}`
     document.getElementById("ClockDisplay").innerHTML = time;
 
     setTimeout(showTime, 1000);   // show running seconds
